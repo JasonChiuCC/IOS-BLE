0020dc6b73bebc86bb5a157df0273198a7087ca4
JasonChiuCC <JasonChiuCC@gmail.com>
2016-08-31 00:33:20 +0800

    Modify view

--- IOS-BLE/ViewController.swift
+++ IOS-BLE/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sendTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendButton(sender: AnyObject) {
+        
+    }
 
 }
 
